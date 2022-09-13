5a2b00638c618cd95e4e6f10dbef70cfb97f9b30
alexandre-dev29 <axel.business29@gmail.com>
2022-09-13 14:49:12 +0200

    adding zustand react-hot-toast and other dependencies and adding some config

---
+++ config/themes/darkTheme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@nextui-org/react";
+
+export const DarkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {},
+    fonts: {
+      sans: "Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;",
+    },
+  },
+});
---
+++ config/themes/lightTheme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@nextui-org/react";
+
+export const LightTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {},
+    fonts: {
+      sans: "Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;",
+    },
+  },
+});
