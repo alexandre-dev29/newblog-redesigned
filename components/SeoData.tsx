b5a704b4f00bc20da267c091c4a30c27d19bf133
alexandre-dev29 <axel.business29@gmail.com>
2022-09-26 17:59:35 +0200

    Resolving error on Seo image component

--- components/SeoData.tsx
+++ components/SeoData.tsx
@@ -22,7 +22,7 @@ export function SeoData({ pageTitle, description, imageUrl, keywords, urlPath }:
       <meta property="og:type" content="article" />
       <meta property="og:title" content={`${pageTitle}`} />
       <meta property="og:description" content={`${description}`} />
-      <meta property="og:image" content={imageFromSource(`${imageUrl}`).url()} />
+      {imageUrl && <meta property="og:image" content={imageFromSource(`${imageUrl}`).url()} />}
     </Head>
   );
 }
